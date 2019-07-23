8236ebe56326ed6f2c2d82d3876813549df04518
Darek Tidwell <48836981+chefdarek@users.noreply.github.com>
2019-07-23 18:13:02 -0500

    Finally up and running

--- tweetseek/app.py
+++ tweetseek/app.py
@@ -1,28 +1,28 @@
 from flask import Flask, render_template
 
 def create_app():
-    """ Create and configure an instance of the Flask application"""
+    """Create and configure an instance of the Flask application"""
     app = Flask(__name__)
 
     #app.route determines the page to route to after "/"
     @app.route('/')
     def root():
-        """render templates autolooks in the template folder"""
+        """render templates autolooks in the template folder."""
 
         return render_template("home.html")
-    
-        return app
 
-    #@app.route("/templates")
-    #def preds():
-        #""" render the home.html template """
-        #return render_template("home.html")
+    @app.route("/templates")
+    def preds():
+        """render the home.html template."""
+        return render_template("home.html")
 
-    #@app.route("/about")
-    #def about():
-        """ render the about.html page """
+    @app.route("/about")
+    def about():
+        """render the about.html page."""
 
-        #return render_template("about.html")
+        return render_template("about.html")
+
+    return app
 
 #if __name__ == "__main__":
  #   app.run(debug=True)
